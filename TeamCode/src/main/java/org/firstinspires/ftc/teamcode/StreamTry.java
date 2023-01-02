33d08f459d3928c8f641d0d37289973936d5799f
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-01-02 16:56:05 +0200

    Fixed Apriltags problem
    Fixed Apriltags problem by resetting all project dependencies

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StreamTry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StreamTry.java
@@ -4,15 +4,15 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvWebcam;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvInternalCamera2;
 
 @TeleOp
-public class StreamTest extends LinearOpMode {
+public class StreamTry extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -20,15 +20,19 @@ public class StreamTest extends LinearOpMode {
         msStuckDetectStop = 2500;
         OpenCvCamera camera = null;
         //int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        try {
-            camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"));
-        } catch (Throwable t) {
-            telemetry.addLine(t.getMessage());
-        }
+//            camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        camera = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK);
+
+        camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
         FtcDashboard.getInstance().startCameraStream(camera,30);
 
+        telemetry.update();
         waitForStart();
 
-        while (opModeIsActive()) ;
+        while (opModeIsActive()){
+            telemetry.addLine(camera.toString());
+            telemetry.update();
+            sleep(3);
+        }
     }
 }
